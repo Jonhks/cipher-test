f22fe091d053926c0414a0b2841a1253822c82ba
jonhks <jonathan@laboratoria.la>
2019-01-14 09:56:33 -0600

    subiendo version con test iniciales pasando. (faltan cosas a mejorar como los Stms 63, branch al 50% y lines al 63)

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,44 @@
 window.cipher = {
-  // ... 
+  encode: (offset, string) => {
+    let wordAscii = '' ;
+    offset = parseInt(offset);
+    if(isNaN(offset)){
+      alert('El campo "Offset" funciona al ingresa números mayores a 0. \n Intenta nuevamente. 🤦🏻‍')
+      return wordAscii;
+    }
+    for(let i = 0 ; i < string.length; i++) {
+      let ascciCode = string.charCodeAt(i);
+      if(ascciCode >= 65 && ascciCode <= 90){
+        let ascciCifrado = (ascciCode - 65 + offset) % 26 + 65;
+        wordAscii += String.fromCharCode(ascciCifrado); 
+      } else if (ascciCode >=97 && ascciCode <= 122){
+				let ascciCifrado = (ascciCode - 97 + offset) % 26 + 97
+        wordAscii += String.fromCharCode(ascciCifrado); 
+      } else if (ascciCode === 32  || ascciCode >= 91 || (ascciCode >=33 || ascciCode<=64)){ 
+        wordAscii += String.fromCharCode(ascciCode); 
+      }
+    }
+   return wordAscii
+  },
+  decode: (offset, string) => {
+    let ascciWord = '';
+    offset = parseInt(offset);
+    if(isNaN(offset)){
+      alert('El campo "Offset" funciona al ingresa números mayores a 0. \n Intenta nuevamente. 🤦🏻‍')
+      return ascciWord; 
+    }
+    for(let i = 0; i <= string.length; i++){
+      let ascciCode = string.charCodeAt(i);
+      if(ascciCode >= 65 && ascciCode <= 90){
+        let ascciCifrado = (ascciCode + 65 - offset) % 26 +65;
+        ascciWord += String.fromCharCode(ascciCifrado)
+      } else if (ascciCode >= 97 && ascciCode <= 122) {
+        let ascciCifrado = (ascciCode - 97 - offset) % 26 + 97;
+        ascciWord += String.fromCharCode(ascciCifrado); 
+      } else if (ascciCode === 32  || ascciCode === 95 || (ascciCode>=33 || ascciCode<=64)){
+        ascciWord += String.fromCharCode(ascciCode); 
+      }
+    }
+    return  ascciWord;
+  }
 };
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -9,10 +9,11 @@ describe('cipher', () => {
     it('debería ser una función', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
-
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
-    //
-    // Hacker edition
+    
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33',() =>{
+      assert.equal(cipher.encode(33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"),"HIJKLMNOPQRSTUVWXYZABCDEFG" );
+   });
+      // Hacker edition
     //
     // Si decides implementar soporte para caracteres no alfabéticos descomenta
     // el test a continuación.
@@ -38,8 +39,9 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
-
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33',() =>{
+      assert.equal(cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG"),"ABCDEFGHIJKLMNOPQRSTUVWXYZ" );
+   });
     //
     // Hacker edition
     //
